5f4dc93413ceef936a560bc7ef2cebaab3681810
Luís Barbosa <luishenriqueb98@gmail.com>
2023-02-01 14:59:54 -0300

    update edit.js

--- js/edit.js
+++ js/edit.js
@@ -1,50 +1,42 @@
-const URL_ATUAL = window.location.search;
-let parametros = new URLSearchParams(URL_ATUAL);
-const ID = parametros.get("id");
+const params = new URLSearchParams(window.location.search)
+const id = params.get("id")
 
-async function events(){
+async function eventId(){
+    let inputName = document.querySelector("#nome")
+    let inputBanner = document.querySelector("#banner")
+    let inputAttractions = document.querySelector("#atracoes")
+    let inputDescription = document.querySelector("#descricao")
+    let inputScheduled = document.querySelector("#data")
+    let inputTickets = document.querySelector("#lotacao")
+
     try{
-    const inputName = document.querySelector("#nome")
-    const inputBanner = document.querySelector("#banner")
-    const inputAttractions = document.querySelector("#atracoes")
-    const inputDescription = document.querySelector("#descricao")
-    const inputSchedule = document.querySelector("#data")
-    const inputTickets = document.querySelector("#lotacao")
-    
-
-    const res = await fetch(`https://soundgarden-api.deta.dev/events/${ID}`, {
-        method: "GET",
-        headers: {
-            "Content-Type" : "application/json",    
-        }
-    })
-
-    const data = await res.json()
+    let res = await fetch(`https://soundgarden-api.deta.dev/events/${id}`)
+    let data = await res.json()
 
     inputName.value = data.name
     inputBanner.value = data.poster
     inputAttractions.value = data.attractions
     inputDescription.value = data.description
-    inputSchedule.value = data.scheduled
+    inputScheduled.value = data.scheduled
     inputTickets.value = data.number_tickets
-    
     }catch(error){
-       console.log(error)
-    }   
+        console.log(error)
+    }
 }
-events(ID)
 
-//Update Event
+eventId()
 
-const eventName = document.querySelector("#nome")
-const attractions = document.querySelector("#atracoes")
-const description = document.querySelector("#descricao")
-const scheduled = document.querySelector("#data")
-const tickets = document.querySelector("#lotacao")
+//update Event
+
 const btn = document.querySelector("#btn")
-
-btn.addEventListener("click", async (event)=>{
+.addEventListener('click', async (event)=>{
     event.preventDefault()
+
+    let eventName = document.querySelector("#nome")
+    let attractions = document.querySelector("#atracoes")
+    let description = document.querySelector("#descricao")
+    let scheduled = document.querySelector("#data")
+    let tickets = document.querySelector("#lotacao")
 
     let raw = {
         "name": eventName.value,
@@ -53,24 +45,21 @@ btn.addEventListener("click", async (event)=>{
             attractions.value
         ],
         "description": description.value,
-        "scheduled": "2022-03-24T00:57:37.761Z",
+        "scheduled": new Date().toISOString(),
         "number_tickets": Number(tickets.value) 
     }
 
     try {
-        const response = await fetch(`https://soundgarden-api.deta.dev/events/${ID}`, {
+        let res = await fetch(`https://soundgarden-api.deta.dev/events/${id}`, {
             method: "PUT",
-            mode: "cors",
             headers: {
-                "Content-Type" : "application/json"
+                "Content-Type" : "application/json",    
             },
             body: JSON.stringify(raw)
         })
-
-        const resData = await response.json()
-
-        console.log(resData)
-        alert("Evento atualizado")
+        let data = await res.json()
+        
+        alert("Evento atualizado!")
     } catch (error) {
         console.log(error)
     }
